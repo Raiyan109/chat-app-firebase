ef9ec1f6fb7d98263523a5246da13c866a99fa4b
Raiyan109 <kraiyan109@gmail.com>
2022-09-23 15:39:15 +0600

    share

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,3 +1,4 @@
+import { EmojiEmotions, Label, PermMedia, Room } from '@mui/icons-material';
 import React from 'react';
 import styles from './Share.module.css'
 
@@ -7,10 +8,33 @@ const Share = () => {
         <div className={styles.share}>
             <div className={styles.shareWrapper}>
                 <div className={styles.shareTop}>
-                    <img className={styles.shareProfileImg} src="../../assets/person/1.jpeg" alt="" />
+                    <img className={styles.shareProfileImg} src="../../assets/person/2.jpeg" alt="" />
                     <input placeholder="What's in your mind Agatha?" className={styles.shareInput} />
                 </div>
-                <div className={styles.shareBottom}></div>
+                <hr className={styles.shareHr} />
+                <div className={styles.shareBottom}>
+                    <div className={styles.shareOptions}>
+                        <div className={styles.shareOption}>
+                            <PermMedia htmlColor='tomato' className={styles.shareIcon} />
+                            <span className={styles.shareOptionText}>Photo or Video</span>
+                        </div>
+                        <div className={styles.shareOption}>
+                            <Label htmlColor='blue' className={styles.shareIcon} />
+                            <span className={styles.shareOptionText}>Tag</span>
+                        </div>
+                        <div className={styles.shareOption}>
+                            <Room htmlColor='green' className={styles.shareIcon} />
+                            <span className={styles.shareOptionText}>Location</span>
+                        </div>
+                        <div className={styles.shareOption}>
+                            <EmojiEmotions htmlColor='goldenrod' className={styles.shareIcon} />
+                            <span className={styles.shareOptionText}>Feelings</span>
+                        </div>
+                    </div>
+
+                    <button className={styles.shareButton}>Share</button>
+                    <div className={styles}></div>
+                </div>
             </div>
         </div>
     );
